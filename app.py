6a77d929b8808eec1ba2ed1d0d35c70246a7e863
Ермоленко Артём <artem.e.ermolenko@gmail.com>
2025-11-14 20:47:11 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
@@ -68,6 +68,10 @@ def counter():
 </html>
 '''
 
+
+@app.route("/info")
+def info():
+    return redirect ("/author")
 
 
         
