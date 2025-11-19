2899d5efe768fc0384a8976061caec2f18bb6227
Ермоленко Артём <artem.e.ermolenko@gmail.com>
2025-11-19 17:56:56 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -459,7 +459,15 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    name = 'Артём Ермоленко'
-    group = 'ФБИ-32'
-    course = '3 курс'
-    return render_template('example.html')
+    name, num, group, course = 'Ермоленко Артём', 2, 'ФБИ-32', 3
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html',
+                            name=name, num=num, group=group,
+                            course=course, fruits=fruits)
+    
