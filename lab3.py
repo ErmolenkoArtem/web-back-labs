dbf2865663b94089853d50e1328ee62398d2ef58
Ермоленко Артём <artem.e.ermolenko@gmail.com>
2025-12-12 13:47:41 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -13,9 +13,9 @@ def lab():
 @lab3.route('/lab3/cookie')
 def cookie():
     resp = make_response(redirect('/lab3/'))
-    resp.set_cookie('name', 'IGOR', max_age = 5)
+    resp.set_cookie('name', 'Alex', max_age = 5)
     resp.set_cookie('age', '20')
-    resp.set_cookie('name_color', 'BLUE')
+    resp.set_cookie('name_color', 'magenta')
     return resp
 
 
@@ -34,8 +34,40 @@ def form1():
     user = request.args.get('user')
     if user == '':
         errors['user'] = 'Заполните поле!'
+
     age = request.args.get('age')
     if age == '':
         errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template('lab3/form1.html', user = user, age = age, sex = sex, errors = errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    # Пусть кофе стоит 120 рублей, чертный чай - 80 рублей, зеленый - 70 рублей.
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    # Добавка молока удорожает напиток на 30 рублей, а сахара - на 10.
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+
+    return render_template('lab3/pay.html', price = price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price')
+    return render_template('lab3/success.html', price = price)
